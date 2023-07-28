2712c7cca5db9a46cf27a7505c35b8b50a0b5446
EvolvingHomosapien <jiang.j.jason@gmail.com>
2023-07-28 09:40:30 -0500

    update quotes controller

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -24,12 +24,11 @@ function newQuote(req, res) {
 }
 
 async function create(req, res) {
-    // Update this line because now we need the _id of the new movie
+    // Update this line because now we need the _id of the new quote
     try { const quote = await Quote.create(req.body);
     // Redirect to the new quote's show functionality 
     res.redirect(`/quotes/${quote._id}`);
   } catch (err) {
-    // Typically some sort of validation error
     console.log(err);
     res.render('quotes/new', { errorMsg: err.message });
   }
